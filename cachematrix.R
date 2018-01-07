60381a747fe655df07e77687e32969545cf08c9c
Paulo Ribeiro <paulo@estrategistas.com>
2018-01-07 20:42:42 -0300

    Here we go. 1st commit to 2nd assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The idea of this project is to save computer processing. 
+## Matrix inversion is a costly calculation, so it makes sense 
+## to reuse results if you've already done that specific computing.
+## The combination of these two functions allows for that.
 
-## Write a short comment describing this function
+## This function takes a matrix and returns a structure to be used
+## by the cacheSolve function. The structure store a matrix and its
+## inverse. The return is a list consisting on 4 functions and
+## 2 objects
 
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get, setinverse = setinverse, 
+           getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## chacheSolve is designed to populate or retrive the Inversion of an
+## object of the type makeCacheMatrix, which is, in itself, 
+## a structured version of the matrix we want to invert.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if (!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data)
+      x$setinverse(m)
+      m
 }
